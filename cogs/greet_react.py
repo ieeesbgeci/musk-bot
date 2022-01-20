4cbe46b1a4c760f19a88eba3a4055d33d9214d53
v4zha <arvin_j4848@protonmail.com>
2022-01-20 23:43:29 +0530

    update

--- cogs/greet_react.py
+++ cogs/greet_react.py
@@ -1,4 +1,9 @@
+from pydoc import describe
+from turtle import color
 from discord.ext import commands
+import discord
+import random
+
 class g_r_mod(commands.Cog):
 	def __init__(self,bot):
 		self.bot=bot
@@ -9,22 +14,29 @@ class g_r_mod(commands.Cog):
 
 	
 	@commands.Cog.listener()
-	async def on_member_join(self,member):
+	async def on_member_join(self,ctx,member):
 		#welcomes new users to server :)
 		channel=member.guild.system_channel
-		await channel.send(f"Sangha Shathiyileeku swagatham {member} Mitramee")
+		cvalue = random.randint(0, 0xffffff)
+		embed  = discord.Embed(description=f"Sangha Shathiyileeku swagatham {member} Mitramee",color=cvalue)
+		file= discord.File("/app/files/pm_modi.jpg")
+		embed.set_image(url="attachment://pm_modi.jpg")
+		await ctx.send(embed=embed)
 
-	@commands.Cog.listener()
-	async def on_reaction_add(self,reaction,user):
-		#notify when users react to messages :)
-		channel=reaction.message.channel
-		async for user in reaction.users():
-			await channel.send(f"<{user.name}> gave reaction to user <{reaction.message.author.name}> with {reaction.emoji}")
+	# @commands.Cog.listener()
+	# async def on_reaction_add(self,reaction,user):
+	# 	#notify when users react to messages :)
+	# 	channel=reaction.message.channel
+	# 	async for user in reaction.users():
+	# 		await channel.send(f"<{user.name}> gave reaction to user <{reaction.message.author.name}> with {reaction.emoji}")
 
 	@commands.command()
 	async def help(self,ctx):
 		#send help message
-		await ctx.send(f"Mem Modi ji Hum.Mere pyari Sanghawasiyoo.\n1.dwajapranam")
+		cvalue = random.randint(0, 0xffffff)
+		embed = discord.Embed(title=" HELP ", description="Mem Modi ji Hum.Mere pyari Sangha_Vashiyoo.",color=cvalue)
+		embed.add_field(name="!dwajapranamam",value="Mitrangale Ashirwadikkum", inline=False)
+		await ctx.send(embed=embed)
 
 	@commands.Cog.listener()
 	async def on_command_error(self,ctx,error): 
@@ -33,7 +45,11 @@ class g_r_mod(commands.Cog):
 
 	@commands.command()
 	async def dwajapranam(self,ctx):
-		await ctx.send(f"Dwajaprenam Mitramee :)");
+		cvalue = random.randint(0, 0xffffff)
+		embed=discord.Embed(description="Dwajaprenam Mitramee :)")
+		file= discord.File("/app/files/pranamam.png")
+		embed.set_image(url="attachment://pranamam.png")
+		await ctx.send(embed=embed)
 
 def setup(bot):
 	bot.add_cog(g_r_mod(bot))	
